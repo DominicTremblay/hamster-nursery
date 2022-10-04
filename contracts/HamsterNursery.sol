de493b0668eecf0041356b88d782cee87a363be8
Dominic Tremblay <dominictremblay24@gmail.com>
2022-10-04 00:40:13 -0400

    creating the hamster type

--- contracts/HamsterNursery.sol
+++ contracts/HamsterNursery.sol
@@ -1,8 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
 contract HamsterNursery {
-    
+  enum ColorType {
+    golden,
+    beige,
+    brown,
+    black,
+    blonde,
+    chocolate,
+    cream,
+    dove,
+    grey,
+    lilac,
+    sable,
+    mink,
+    rust,
+    white
+  }
+  enum PatternType {
+    banded,
+    dominantspot,
+    tortoiseshell,
+    recessivedappled,
+    cinnamon
+  }
+
+  // creating the Hamster type
+  struct Hamster {
+    string name;
+    ColorType color;
+    PatternType pattern;
+  }
+
+  // declare a dynamic array of hamsters
+  Hamster[] public hamsters;
 }
